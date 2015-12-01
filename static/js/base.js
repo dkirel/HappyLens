1b766e8116a31e50fd2be72038737995d652c31a
Daniel Kirel <kireldaniel@gmail.com>
2015-12-01 18:43:14 +0000

    Modify deck.js navigation to wrap-around

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
-    /*
-    $("a").removeAttr("onclick");
-    $("a").prop("onclick", null);
-    */
+
+
 });
 
 
--- static/js/project.js
+++ static/js/project.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
 
-    $.deck('enableScale')
-    $.deck('showMenu')
+    /* Masonry JS */
 
-/*
-    var containerHeight = $('.deck-container').height();
-    var imgHeight = 0;
+    var leftRightMargin = 0.1
+    var imagesPerRow = 3;
+    var paddingBetweenImages = 0.01
+    var columnWidth = Math.round($('.grid').innerWidth()/imagesPerRow);
+    var deckIsInitialized = false;
 
-    //Resize images
-    $('.fit-box').each(function(i, img) {
-
-        imgHeight = img.height();
-        img.width();
-
-
-        }
-    );
-
-    $( "div" ).each(function( index, element ) {
-        // element == this
-        $( element ).css( "backgroundColor", "yellow" );
-        if ( $( this ).is( "#stop" ) ) {
-          $( "span" ).text( "Stopped at div index #" + index );
-          return false;
-        }
+    var $grid = $('.grid').masonry({
+        columnWidth: columnWidth,
+        isFitWidth: true
     });
-*/
+
+    $('.grid-item').width(columnWidth - 0.01*$('body').width());
+
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress(function() {
+        $grid.masonry('layout');
+    });
+
+
+    /* Deck.js JS */
+
+    //Activate and/or show deck
+    $(".grid-item").click(function() {
+
+        if (deckIsInitialized == false) {
+            $.deck('.slide');
+            deckIsInitialized = true;
+        }
+
+        jQuery.fx.off = true;
+        $.deck('go', $(this).attr('id'));
+        $('#toggle-visibility').show();
+        jQuery.fx.off = false;
+
+    });
+
+    //Hide deck
+    $(".exit-deck").click(function() {
+        $('#toggle-visibility').hide();
+    });
+
 
     console.log('Project.js file was successfully loaded!');
 
